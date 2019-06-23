451444f4745996c360254aff26f7a540b84ebac8
Kasie <kchaplick92@gmail.com>
2019-06-23 15:12:45 -0400

    Updates

---
+++ precache-manifest.d748f0d85c068fcee6796d447f6253f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a8874f97242b74fbe7223ba0f8d568",
+    "url": "/PotterMemoryGame/index.html"
+  },
+  {
+    "revision": "0fec2b8a3139895b9108",
+    "url": "/PotterMemoryGame/static/css/main.242632b1.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/PotterMemoryGame/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "0fec2b8a3139895b9108",
+    "url": "/PotterMemoryGame/static/js/main.4538f2ea.chunk.js"
+  },
+  {
+    "revision": "a5dfee5a1e9050c1d900",
+    "url": "/PotterMemoryGame/static/js/runtime~main.9522ac36.js"
+  }
+]);
